7765f7605951ec29459f57c39a67387bf5c296b7
Mounit <mounitmankotia21@gmail.com>
2026-06-17 23:39:28 +0530

    utc

--- routers.py
+++ routers.py
@@ -7,4 +7,6 @@ from config import CACHE_TIME
 router = APIRouter()
 
 @router.get("/news")
-async def getnews(backgroundtask: BackgroundTasks)
+async def getnews(backgroundtask: BackgroundTasks):
+    now = datetime.utcnow()
+    
